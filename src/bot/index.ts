8743c28fbf51766c1c7a6e8c3c710da5d80de8e8
Nicolás Passerini <npasserini@gmail.com>
2024-11-24 23:55:53 -0300

    Entrena con las jugadas previas también

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -2,7 +2,7 @@ import { getEasyMove } from './easy'
 import { getMediumMove } from './medium'
 import { getHardMove } from './hard'
 import { getMLMove, initializeModel, trainModel } from './ml'
-import { BoardState } from '../game/board'
+import { BoardState, Squares } from '../game/board'
 import logRewards from '../util/logRewards'
 import { Move } from '../game/useTicTacToe'
 
@@ -21,36 +21,47 @@ export const initializeBot = async (): Promise<void> => {
   await initializeModel()
 }
 
-// Asume que el último en jugar fue el humano, la computadora empató o perdió.
+// Asume que el último en jugar fue el humano, la computadora perdió.
 export const handleEndGame = (board: BoardState, moves: Move[], level: Level) => {
   if (!level.startsWith('ML')) return
 
+  const trainingData: number[][] = []
+  const targetData: number[][] = []
+
   // const sublevel = Number(level[2])
-  rewardLastComputerMove(moves)
+
+  var reward = 1
+  for (var index = moves.length - 1; index > 0; index -= 2) {
+    // moves[index] tiene la última jugada del contrario (que gana)
+    const oppositeMove = moves[index].lastMove
+
+    // moves[index - 1] tiene la última jugada propia (perdedora)
+    const squares = moves[index - 1].board.squares
+    const ownMove = moves[index - 1].lastMove
+
+    const { training, targets } = punishMove(squares, ownMove, oppositeMove, reward)
+    trainingData.push(training)
+    targetData.push(targets)
+
+    // La última jugada se penaliza al máximo, cada jugada se reduce un 50% la penalidad
+    reward /= 2
+  }
+
+  setTimeout(() => trainModel(trainingData, targetData), 0)
 }
 
-const rewardLastComputerMove = (moves: Move[]) => {
-  // Asume 'O' = bot / 'X' = humano
-  console.log(moves)
-  const { winner } = moves[moves.length - 1].board
+const punishMove = (squares: Squares, ownMove: number, oppositeMove: number, reward: number) => {
+  const training = squares.map(sq => (sq === 'X' ? 1 : sq === 'O' ? -1 : 0))
 
-  if (winner === 'X') {
-    // Castigo por perder
-    const squares = moves[moves.length - 2].board.squares
-    const training = squares.map(sq => (sq === 'X' ? 1 : sq === 'O' ? -1 : 0))
+  const targets = Array(9).fill(0)
 
-    const targets = Array(9).fill(0)
+  // Penalizar lo que jugó
+  targets[ownMove] = -reward
 
-    // Penalizar lo que jugó
-    const lastOwnMove = moves[moves.length - 2].lastMove
-    targets[lastOwnMove] = -1
+  // Premiar lo que debió haber jugado.
+  targets[oppositeMove] = reward
 
-    // Premiar lo que debió haber jugado.
-    const lastOppositeMove = moves[moves.length - 1].lastMove
-    targets[lastOppositeMove] = 1
+  logRewards(squares, targets)
 
-    logRewards(squares, targets)
-
-    setTimeout(() => trainModel([training], [targets]), 0)
-  }
+  return { training, targets }
 }
